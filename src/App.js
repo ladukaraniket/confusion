b2f1eb1b0905f5b11dc787f1fe312e05224103dc
Aniket Ladukar <ladukaraniket@gmail.com>
2020-07-06 16:33:17 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
 
 
-  render(){
+  render() {
     return (
-      <div>
-        <Main></Main>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main></Main>
+        </div>
+      </BrowserRouter>
     );
   }
-  
+
 }
 
 export default App;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,35 +3,42 @@ import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import {DISHES} from '../shared/dishes';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       dishes: DISHES,
-      selectedDish:null
-    };    
+    };
   }
-  
-  onDishSelect(dishId){
-    this.setState({
-        selectedDish:dishId
-    });
-  }
-  render(){
+
+
+  render() {
+
+    const HomePage= () => {
+      return(
+        <Home></Home>
+      );
+    };
+
     return (
       <div>
         <Header></Header>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}></Menu>
-        <DishDetail dishDetail={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} ></DishDetail>
+        <Switch>
+          <Route path="/home" component={HomePage}></Route>
+          <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}></Menu>}></Route>
+          <Redirect to="/home"></Redirect>
+        </Switch>
         <Footer></Footer>
       </div>
     );
   }
-  
+
 }
 
 export default Main;
