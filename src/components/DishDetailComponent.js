3c5daa63362ee2877d153f81e158f5f33db84771
Aniket Ladukar <ladukaraniket@gmail.com>
2020-07-22 16:33:09 +0530

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = (val) => val && val.length;
@@ -103,7 +104,7 @@ function RenderDish({ dish }) {
     return (
         <div className="col-12 col-md-5 mt-1">
             <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}></CardImg>
                 <CardBody>
                     <CardTitle >{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,7 +1,34 @@
-import { PROMOTIONS } from '../shared/promotions';
 
-export const Promotions = (state = PROMOTIONS, action) => {
-    switch(action){
+import * as ActionTypes from './ActionTypes';
+
+export const Promotions = (state = {
+    isLoading:true,
+    errmess: null,
+    promotions: []
+}, action) => {
+    switch (action.type) {
+        case ActionTypes.PROMOS_LOADING:
+            return {
+                ...state,
+                isLoading: true,
+                errmess: null,
+                promotions: []
+            }
+
+        case ActionTypes.PROMOS_FAILED:
+            return {
+                ...state,
+                isLoading: false,
+                errmess: action.payload,
+            }
+
+        case ActionTypes.ADD_PROMOS:
+            return {
+                ...state,
+                isLoading: false,
+                errmess: null,
+                promotions: action.payload
+            }
         default:
             return state;
     }
